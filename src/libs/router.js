be06409b688bfcf0672ed08a1725e79eb31aa2b1
Kittihengcharoen Chaemprasert <KINNOM-Cyber@users.noreply.github.com>
2025-06-25 19:35:30 +0700

    feat: implement initial structure for project pages

--- src/libs/router.js
+++ src/libs/router.js
@@ -2,6 +2,14 @@ import { createBrowserRouter } from "react-router";
 
 import HomePage from "@/pages/home/page";
 import RootLayout from "@/pages/layout";
+import LoginPage from "@/pages/login/page";
+import ProfilePage from "@/pages/profile/page";
+import TinderGamePage from "@/pages/tinder/page";
+import ScheduleLandingPage from "@/pages/course/schedules/page";
+import {
+  houseScheduleLoader,
+  HouseSchedulePage,
+} from "@/pages/course/schedules/[house-id]/page";
 
 export const router = createBrowserRouter([
   {
@@ -11,6 +19,27 @@ export const router = createBrowserRouter([
       {
         index: true,
         Component: HomePage,
+      },
+      {
+        path: "/login",
+        Component: LoginPage,
+      },
+      {
+        path: "/profile",
+        Component: ProfilePage,
+      },
+      {
+        path: "/tinder",
+        Component: TinderGamePage,
+      },
+      {
+        path: "/course/schedules",
+        Component: ScheduleLandingPage,
+      },
+      {
+        path: "/course/schedule/:houseId",
+        Component: HouseSchedulePage,
+        loader: houseScheduleLoader,
       },
     ],
   },
