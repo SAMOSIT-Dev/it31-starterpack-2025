01bdb27b458214e5c2c962cfa29b3f84ce46a52c
Kittihengcharoen Chaemprasert <KINNOM-Cyber@users.noreply.github.com>
2025-06-29 15:35:41 +0700

    BE (Add): Add CORS middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,15 +1,20 @@
 const express = require("express");
+const cors = require("cors");
+
 const AppConfig = require("./config/app.config");
+
 const HouseRouter = require("./routers/house.router");
 const UserRouter = require("./routers/users.router");
 const UpcomingEventRouter = require("./routers/upcoming-event.router");
 const ScheduleRouter = require("./routers/schedule.router");
 const CourseRouter = require("./routers/course.router");
 const RoomRouter = require("./routers/room.router");
+
 const app = express();
 const path = require("path");
 
 app.use(express.json());
+app.use(cors());
 
 app.use(`${AppConfig.applicationContext}/users`, UserRouter);
 app.use(`${AppConfig.applicationContext}/houses`, HouseRouter);
