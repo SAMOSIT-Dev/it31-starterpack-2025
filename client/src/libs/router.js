668d5cf69c9cc7c75dd4bfb19b328d3f0f34cef8
Napat Jinsuksomboon <boolin.work@gmail.com>
2025-07-13 19:44:31 +0700

    Revert "Merge branch 'dev' into frontend/feature/tinder-page"
    This reverts commit 4872507f0aaa8cd49aa7479f7877599d9e7d3ef2, reversing
    changes made to 0cf8cbf6d746d38b9ae8fb8e9a1531742355397d.

--- client/src/libs/router.js
+++ client/src/libs/router.js
@@ -6,8 +6,10 @@ import LoginPage from "@/pages/login/page";
 import ProfilePage from "@/pages/profile/page";
 import TinderGamePage from "@/pages/tinder/page";
 import ScheduleLandingPage from "@/pages/course/schedules/page";
-import HouseSchedulePage from "@/pages/course/schedules/[house-id]/page";
-import { AuthProvider } from "@/contexts/AuthContext";
+import {
+  houseScheduleLoader,
+  HouseSchedulePage,
+} from "@/pages/course/schedules/[house-id]/page";
 import RootLayoutWithAuth from "@/layout/RootLayoutWithAuth";
 import NotFoundPage from "@/pages/notfound/page";
 
@@ -15,7 +17,7 @@ export const router = createBrowserRouter([
   // 🟢 Routes No AuthProvider
   {
     path: "/",
-    Component: RootLayoutWithAuth,
+    Component: RootLayout,
     children: [
       { index: true, Component: HomePage },
       { path: "login", Component: LoginPage },
@@ -29,11 +31,11 @@ export const router = createBrowserRouter([
     children: [
       { path: "profile", Component: ProfilePage },
       { path: "tinder", Component: TinderGamePage },
-      { path: "course/houses", Component: ScheduleLandingPage },
+      { path: "course/schedules", Component: ScheduleLandingPage },
       {
-        path: "course/houses/:houseId/schedule",
+        path: "course/schedule/:houseId",
         Component: HouseSchedulePage,
-        // loader: houseScheduleLoader,
+        loader: houseScheduleLoader,
       },
     ],
   },
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,20 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { QueryClientProvider } from '@tanstack/react-query';
-import { queryClient } from "./libs/queryClient.js";
 import Modal from "react-modal";
+
+
 import App from "./App.jsx";
 import Providers from "./components/Providers.jsx";
+
 import "./index.css";
 
 Modal.setAppElement('#root');
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <Providers>
+    <Providers>
         <App />
-      </Providers>
-    </QueryClientProvider>
+    </Providers>
   </StrictMode>
 );
