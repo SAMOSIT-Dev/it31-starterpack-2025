4ba29e0e6ed60900693ca56748d6782b17520983
Melbromss <melbromss@gmail.com>
2025-07-16 18:39:35 +0700

    fix houser order

--- client/src/contexts/AuthContext.jsx
+++ client/src/contexts/AuthContext.jsx
@@ -7,26 +7,26 @@ export function AuthProvider({ children }) {
   const [user, setUser] = useState(null); 
   const navigate = useNavigate();
 
-  const fetchUser = async () => {
-    try {
-      const response = await getUserDetail();
+  // const fetchUser = async () => {
+  //   try {
+  //     const response = await getUserDetail();
       
-      const data = response.content;
+  //     const data = response.content;
 
-      const avatar = data.profile_picture_url
-        ? await fetchProfileImage(data.profile_picture_url)
-        : "/common/default_avartar.jpg";
+  //     const avatar = data.profile_picture_url
+  //       ? await fetchProfileImage(data.profile_picture_url)
+  //       : "/common/default_avartar.jpg";
 
-      setUser({ ...data, avatar });
-    } catch (err) {
-      navigate("/login"); 
-    }
-  };
-  useEffect(() => {
-    fetchUser();
-  }, []);
+  //     setUser({ ...data, avatar });
+  //   } catch (err) {
+  //     navigate("/login"); 
+  //   }
+  // };
+  // useEffect(() => {
+  //   fetchUser();
+  // }, []);
   return (
-    <AuthContext.Provider value={{ user , fetchUser}}>
+    <AuthContext.Provider value={{ user}}>
       {children}
     </AuthContext.Provider>
   );
