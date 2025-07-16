c43d1b89ba076271e4f8329681d8f984d082e4e4
Napat Jinsuksomboon <boolin.work@gmail.com>
2025-07-16 23:24:40 +0700

    FE (fix): bring back auth

--- client/src/contexts/AuthContext.jsx
+++ client/src/contexts/AuthContext.jsx
@@ -7,24 +7,24 @@ export function AuthProvider({ children }) {
   const [user, setUser] = useState(null); 
   const navigate = useNavigate();
 
-  // const fetchUser = async () => {
-  //   try {
-  //     const response = await getUserDetail();
+  const fetchUser = async () => {
+    try {
+      const response = await getUserDetail();
       
-  //     const data = response.content;
+      const data = response.content;
 
-  //     const avatar = data.profile_picture_url
-  //       ? await fetchProfileImage(data.profile_picture_url)
-  //       : "/common/default_avartar.jpg";
+      const avatar = data.profile_picture_url
+        ? await fetchProfileImage(data.profile_picture_url)
+        : "/common/default_avartar.jpg";
 
-  //     setUser({ ...data, avatar });
-  //   } catch (err) {
-  //     navigate("/login"); 
-  //   }
-  // };
-  // useEffect(() => {
-  //   fetchUser();
-  // }, []);
+      setUser({ ...data, avatar });
+    } catch (err) {
+      navigate("/login"); 
+    }
+  };
+  useEffect(() => {
+    fetchUser();
+  }, []);
   return (
     <AuthContext.Provider value={{ user}}>
       {children}
